ec990eb94d93e571f55fdac8e5da6bee2e524b34
Shindo Strzelczyk <shindostrz@gmail.com>
2014-03-28 16:02:07 -0700

    Added accepts_nested_attributes_for responses

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -3,11 +3,12 @@ class Convo < ActiveRecord::Base
                           :genders_attributes, :categories_attributes, :category_ids, :gender_ids
 
   belongs_to :user
+  has_many :responses
   has_many :users, through: :responses
   has_many :gender_assignments, as: :genderable
   has_many :genders, through: :gender_assignments
   has_many :categorizations
   has_many :categories, through: :categorizations
 
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, :responses
 end
