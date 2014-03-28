8ac7308bfe86128240c6ee368dcc09cafa6c4b44
Shindo Strzelczyk <shindostrz@gmail.com>
2014-03-27 23:37:49 -0700

    Altered the create convo link for new nested routes and refactored

--- app/assets/javascripts/convos.js
+++ app/assets/javascripts/convos.js
@@ -7,25 +7,25 @@ window.onload = function () {
         map.locate({setView: true, maxZoom: 16});
 
         for (var index in gon.coordinates) {
+          if (gon.coordinates[index][0]) {
           L.marker(gon.coordinates[index]).addTo(map).bindPopup("<strong>" + gon.category[index] + "</strong><br>" + gon.description[index] + "<br><a href='/convos/" + [index] + "'>Go to convo</a>");
-
+        }
         };
 
   var marker;
         map.on('click', function(e){
+          new_convo_url ="<a href='users/" + gon.current_user.id +"/convos/new/?lat=" + e.latlng["lat"] + "&lng=" +e.latlng["lng"] + "'>" + "Start a new convo here</a>";
           // removes last marker if a new marker is dropped
           if (marker) {
             map.removeLayer(marker);
             marker = new L.Marker(e.latlng, {draggable:true});
             map.addLayer(marker);
-            new_convo_url ="<a href='/convos/new/?lat=" + e.latlng["lat"] + "&lng=" +e.latlng["lng"] + "'>" + "Start a new convo here</a>";
             marker.bindPopup(new_convo_url).addTo(map).openPopup();
-        } else {
-          marker = new L.Marker(e.latlng, {draggable:true});
-          map.addLayer(marker);
-          new_convo_url ="<a href='/convos/new/?lat=" + e.latlng["lat"] + "&lng=" +e.latlng["lng"] + "'>" + "Start a new convo here</a>";
-          marker.bindPopup(new_convo_url).addTo(map).openPopup();
-        }
+          } else {
+            marker = new L.Marker(e.latlng, {draggable:true});
+            map.addLayer(marker);
+            marker.bindPopup(new_convo_url).addTo(map).openPopup();
+          }
 
         // makes marker draggable and opens a new popup
         marker.on('dragend', function(e) {
