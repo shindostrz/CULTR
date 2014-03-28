e7f85cef3b3a0426731e29adf858ea9696e72b1f
Shindo Strzelczyk <shindostrz@gmail.com>
2014-03-27 22:10:07 -0700

    Nested convos under users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ CULTR::Application.routes.draw do
 
   root to: 'convos#index'
 
-  resources :users, except: :index
-
-  resources :convos
+  resources :users, except: :index do
+    resources :convos
+  end
 
   get '/response/:id' => 'convos#create_response'
 
-  delete '/response/:id/destroy' => 'convos_users#destroy'
+  delete '/response/:id/destroy' => 'responses#destroy'
 
   resources :sessions, only: [:new, :create]
 
