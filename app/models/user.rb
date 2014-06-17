677b4ce016fb73ec1917ec71b079cb982ee1f8f2
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-17 15:01:46 -0700

    Added from_omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,17 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
   end
