f389fff799e37d980a56f22ee1b1ae4733fed5bc
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-16 23:07:35 -0700

    Added matching routes for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CULTR::Application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
 
   root to: 'convos#index'
 
