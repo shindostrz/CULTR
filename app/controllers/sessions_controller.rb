32a046a19e37680cabc1dc5bda2204c5c7831bec
Shindo Strzelczyk <shindostrz@gmail.com>
2014-06-16 23:07:04 -0700

    Commented out the old sessions code and added omniauth method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
-      sign_in(user)
-      redirect_to root_path
-    else
-      redirect_to new_session_path
-    end
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
+    # user = User.find_by_email(params[:user][:email])
+    # if user && user.authenticate(params[:user][:password])
+    #   sign_in(user)
+    #   redirect_to root_path
+    # else
+    #   redirect_to new_session_path
+    # end
   end
 
   def destroy
-    sign_out
-    redirect_to new_session_path
+    session[:user_id] = nil
+    redirect_to root_path
+    # sign_out
+    # redirect_to new_session_path
   end
 
 end
